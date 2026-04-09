a87972212c494974deaebc57f3cb7d807ca2bd7c
kimjoy2002 <kimjoy2002@naver.com>
2026-04-09 16:46:40 +0900

    feat: 고대카드+산탄 선구현

--- StS2HoshinoCode/Cards/Ancient/FullBarrelFirePlus.cs
+++ StS2HoshinoCode/Cards/Ancient/FullBarrelFirePlus.cs
@@ -15,7 +15,7 @@ using StS2Hoshino.StS2HoshinoCode.Utils;
 namespace StS2Hoshino.StS2HoshinoCode.Cards.Basic;
 
 
-public class FullBarrelFire() : StS2HoshinoCard(2, CardType.Attack, CardRarity.Basic, TargetType.AnyEnemy)
+public class FullBarrelFire() : StS2HoshinoCard(1, CardType.Attack, CardRarity.Ancient, TargetType.AnyEnemy)
 {
     public override int AmmoCost { get; set; } = 1;
 
@@ -24,20 +24,30 @@ public class FullBarrelFire() : StS2HoshinoCard(2, CardType.Attack, CardRarity.B
         HoverTipFactory.FromKeyword(HoshinoKeywords.Bullet)
     ];
     protected override HashSet<CardTag> CanonicalTags => [];
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(4, ValueProp.Move)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(6, ValueProp.Move)];
 
     protected override async Task OnHoshinoPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
         ArgumentNullException.ThrowIfNull(play.Target, "play.Target");
-
+        
+        Creature singleTarget = play.Target;
         int extraAmount = AmmoClass.LoseAmmo(99, ((CardModel)this).Owner);
         int amount = extraAmount + AmmoCost;
         for (; amount > 0; amount--)
         {
-            if (!play.Target.IsAlive)
-                break;
+            if (!singleTarget.IsAlive) {
+                List<Creature> validTargets = base.CombatState.HittableEnemies.Where<Creature>((Func<Creature, bool>) (c => c.IsAlive)).ToList<Creature>();
+                if (validTargets.Count > 0)
+                {
+                    singleTarget = RunState.Rng.CombatTargets.NextItem<Creature>((IEnumerable<Creature>) validTargets);
+                    if (singleTarget == null)
+                    {
+                        break;
+                    }
+                }
+            }
             
-            await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).FromCard(this).Targeting(play.Target).Execute(choiceContext);
+            await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).FromCard(this).Targeting(singleTarget).Execute(choiceContext);
 
             //총알 사용
             IEnumerable<IBulletPowerInterface> enumerable = base.Owner.Creature.Powers.OfType<IBulletPowerInterface>();
@@ -45,11 +55,16 @@ public class FullBarrelFire() : StS2HoshinoCard(2, CardType.Attack, CardRarity.B
             {
                 item.UseBullet(this, play.Target,base.Owner.Creature, 1);
             }
+            
+            if(!singleTarget.IsAlive) {
+                await ReloadCmd.Execute(choiceContext, base.Owner);
+                amount = AmmoClass.LoseAmmo(99, ((CardModel)this).Owner);
+            }
         }
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Damage.UpgradeValueBy(1m);
+        DynamicVars.Damage.UpgradeValueBy(2m);
     }
 }
---
+++ StS2HoshinoCode/Powers/BulletIncendiaryPower.cs
@@ -0,0 +1,46 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using StS2Hoshino.StS2HoshinoCode.Hook;
+
+namespace StS2Hoshino.StS2HoshinoCode.Powers;
+
+
+public sealed class BulletIncendiaryPower : StS2HoshinoPower, IOnReloaded, IBulletPowerInterface
+{
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+
+    public async Task OnReload(PlayerChoiceContext ctx, Player player, bool useButton)
+    {
+        await PowerCmd.ModifyAmount(this, -Amount, null, null);
+    }
+    
+    
+    private async void _internal_useBullet(CardModel card, Creature? applier, int amount) {        
+        for(;amount > 0; amount--) {
+            Flash();
+            await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).FromCard(this).TargetingAllOpponents(base.CombatState)
+                .WithHitFx("vfx/vfx_heavy_blunt", null, "blunt_attack.mp3")
+                .Execute(choiceContext);
+        }
+    }
+
+    public async void UseBullet(CardModel card, Creature? target, Creature? applier, int amount)
+    {
+        _internal_useBullet(card, applier, amount);
+    }
+    
+    public async void UseBulletForMulti(CardModel card, IEnumerable<Creature> targets, Creature? applier, int amount)
+    {
+        _internal_useBullet(card, applier, amount);
+    }
+}
