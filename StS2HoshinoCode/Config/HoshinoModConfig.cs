7a878b6d92aa1954452da3c084df084b8aab9a36
kimjoy2002 <kimjoy2002@naver.com>
2026-05-23 02:00:23 +0900

    장전 버튼 설정 추가

---
+++ StS2HoshinoCode/Config/HoshinoModConfig.cs
@@ -0,0 +1,26 @@
+﻿using BaseLib.Config;
+
+namespace StS2Hoshino.StS2HoshinoCode.Config;
+
+[ConfigHoverTipsByDefault]
+public class HoshinoModConfig : SimpleModConfig
+{
+    
+    public enum ReloadButtonPosition
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    };
+    
+    [ConfigSection("ReloadSettings")]
+    
+    [ConfigHoverTip]
+    public static bool IsVewingReloadButton { get; set; } = true;
+    
+    [ConfigVisibleIf(nameof(IsVewingReloadButton))]
+    [ConfigHoverTip]
+    public static ReloadButtonPosition ButtonPosition { get; set; } = ReloadButtonPosition.Up;
+    
+}
