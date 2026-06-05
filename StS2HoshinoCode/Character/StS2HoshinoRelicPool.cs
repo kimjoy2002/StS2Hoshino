6030b6dfecfd37c415811aa1a20d84ae2d97aecc
kimjoy2002 <kimjoy2002@naver.com>
2026-06-05 15:32:35 +0900

    이미지 추가

--- StS2HoshinoCode/Character/StS2HoshinoRelicPool.cs
+++ StS2HoshinoCode/Character/StS2HoshinoRelicPool.cs
@@ -8,6 +8,6 @@ public class StS2HoshinoRelicPool : CustomRelicPoolModel
 {
     public override Color LabOutlineColor => StS2Hoshino.Color;
 
-    public override string BigEnergyIconPath => "charui/big_energy.png".ImagePath();
-    public override string TextEnergyIconPath => "charui/text_energy.png".ImagePath();
+    public override string BigEnergyIconPath => "charui/hoshino_energy.png".ImagePath();
+    public override string TextEnergyIconPath => "charui/hoshino_text_energy.png".ImagePath();
 }
